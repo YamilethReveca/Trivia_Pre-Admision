abd9f67befa6a663cdb85b404133acc3490248a3
yamileth caldera <yamilethcaldera@gmail.com>
2025-03-05 00:37:20 -0300

    Mejorando juego Trivia

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
+// variables de las respuestas correctas
+
 const respuesta_correcta_1 = "Austria";
 const respuesta_correcta_2 = "1945";
 const respuesta_correcta_3 = "Thomas-Edison";
 const respuesta_correcta_1_2 = "Nopal";
 const respuesta_correcta_2_2 = "Pollo";
-const respuesta_correcta_3_2_2 = "Chile";
+const respuesta_correcta_3_2 = "Chile";
 let temporizador;
 let identificadorTemporizador;
 let puntaje = 0;
 let acierto = 0;
 
-function validar() {
+let respuesta_seleccionada = "";
 
-  let nombre = document.getElementById("nombreUsuario").value;
-
-  if (nombre != "") {
-    document.getElementById("ver-nombre-usuario").innerHTML = ("Bienvenido al juego " + nombre);
-
-  } else {
-
-    alert("No digitó su nombre")
-  }
-  document.getElementById("contenedor-2").style.display = "block";
-  document.getElementById("contenedor-1").style.display = "none";
+// ingrersar al juego index-bienvenida
+function ingresar() {
+  document.getElementById("ingresar")
+  window.location.href = "index-bienvenida.html";
 
 }
 
-function cultura() {
+// ir a la siguiente vista mostrar las alternativas y mostar el nombre del usuario
+function validarNombre() {
+  let nombreUsuario = document.getElementById('nombreUsuario').value.trim(); // Obtiene el valor del input y quita espacios
 
+  if (nombreUsuario !== "") {
+    localStorage.setItem("nombreUsuario", nombreUsuario); // Guarda el nombre en localStorage
+    window.location.href = "index-opciones.html"; // Redirige a la otra vista
+  } else {
+    alert("Por favor, ingresar su nombre.");
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let nombreGuardado = localStorage.getItem("nombreUsuario"); // Obtiene el nombre guardado
+
+  if (nombreGuardado) {
+    // Convierte la primera letra en mayúscula y concatena el resto
+    let nombreCapitalizado = nombreGuardado.charAt(0).toUpperCase() + nombreGuardado.slice(1).toLowerCase();
+
+    document.getElementById("ver-nombre-usuario").innerHTML = `Bienvenid@ ${nombreCapitalizado}, elige 1 de nuestra 4 opciones`;
+  }
+});
+
+// ir según la opción seleccionada
+function cultura() {
   temporizador = 61;
 
   identificadorTemporizador = setInterval(function () {
@@ -40,301 +59,184 @@ function cultura() {
 
   }, 1000);
 
-  document.getElementById("culturaGeneral").style.display = "block"
-  document.getElementById("cultura_general_1").style.display = "block";
-  document.getElementById("contenedor-2").style.display = "none";
+
+
+  
+    let categoria = document.getElementById('boton-1').dataset.category;
+    if (categoria === 'cultura') {
+      window.location.href = "index-cultura.html";
+    }
+  
+
+
+  let boton1 = document.getElementById('boton-1').value;
+  if (boton1 === 'cultura') {
+    window.location.href = "index-cultura.html";
+  }
+
+
+
+
+  document.getElementById('one').style.display = "block"
+  document.getElementById("two").style.display = "none"; // oculyo el contenedor-2
 
 }
+
 
 function gastronomia() {
+  let categoria = document.getElementById('boton-2').dataset.category;
+  
+  if (categoria === 'gastronomia') {
 
-  temporizador = 61;
+    window.location.href = "index-gastronomia.html";
+  }
+}
 
-  identificadorTemporizador = setInterval(function () {
-    --temporizador;
-    document.getElementById("tiempo1_2").innerHTML = temporizador;
+function programacion() {
+  let categoria = document.getElementById('boton-3').dataset.category;  
+  
+  if (categoria === 'programacion') {
 
-    if (temporizador <= 0) {
-      clearInterval(identificadorTemporizador);
-    }
+    window.location.href = "index-programacion.html";
+  }
+}
 
+function conduccion() {
+  let categoria = document.getElementById('boton-4').dataset.category;  
+  
+  if (categoria === 'conduccion') {
 
-  }, 1000);
-
-
-  document.getElementById("gastronomia").style.display = "block";
-  document.getElementById("gastronomia-1").style.display = "block";
-  document.getElementById("contenedor-2").style.display = "none";
+    window.location.href = "index-conduccion.html";
+  }
 
 }
 
+// obtener el valor de cada pregunta
 
+document.querySelectorAll('.opcion').forEach(opcion => {
+
+  opcion.addEventListener('click', function () {
+
+    respuesta_seleccionada = this.value
+
+    console.log('valor seleccionado', this.value);
+
+
+  })
+
+
+})
+
+// validar respuesta primera pregunta cultura
 function validar_respuesta_1() {
-
   clearInterval(identificadorTemporizador);
   temporizador = 61;
-
-
-  let respuesta_seleccionada1 = document.querySelector("input[name=pais]:checked").value;
 
   let guardando = document.querySelector("#ver1");
 
-  if (respuesta_seleccionada1 === respuesta_correcta_1) {
-
-    guardando.innerHTML = ("Correcto!!!, la respuesta es: " + respuesta_seleccionada1);
-    document.getElementById("ver1").style.color = "green";
-
-    puntaje += 100;
-    document.getElementById("puntos").innerHTML = puntaje + " " + "puntos";
-
-    acierto += 1;
-    document.getElementById("acierto-1").innerHTML = acierto + "/3";
-
-  } else {
-
-    guardando.innerHTML = ("Incorrecto, la respuesta correcta es: " + respuesta_correcta_1)
-    document.getElementById("ver1").style.color = "red";
-
-
-
+  // Si no se ha seleccionado ninguna respuesta, mostrar alerta
+  if (!respuesta_seleccionada) {
+    guardando.innerHTML = "⚠️ Por favor, selecciona una respuesta antes de validar.";
+    document.getElementById("ver1").style.color = "orange";
+    return;
   }
 
+  if (respuesta_seleccionada === respuesta_correcta_1) {
+    guardando.innerHTML = `✅ Correcto!!!, la respuesta es: ${respuesta_seleccionada}`;
+    document.getElementById("ver1").style.color = "green";
+    puntaje += 100;
+    document.getElementById("puntos").innerHTML = puntaje + " puntos";
+    acierto += 1;
+    document.getElementById("acierto-1").innerHTML = acierto + "/3";
+  } else {
+    guardando.innerHTML = `❌ Incorrecto, la respuesta correcta es: ${respuesta_correcta_1}`;
+    document.getElementById("ver1").style.color = "red";
+  }
 }
 
-function siguiente_1() {
+// boton ir a la siguiente pregunta
+document.getElementById('siguiente').onclick = function() {
+  document.getElementById('one').style.display = "none";
+  document.getElementById("two").style.display = "block";
+};
 
 
-  document.getElementById("cultura_general_2").style.display = "block";
-  document.getElementById("cultura_general_1").style.display = "none";
 
-  identificadorTemporizador = setInterval(function () {
-    --temporizador;
-    document.getElementById("tiempo2").innerHTML = temporizador;
-    if (temporizador === 0) {
-      clearInterval(identificadorTemporizador);
-    }
+// se la segunda pregunta de cultura general
 
-
-  }, 1000);
-
-
-}
-
+// validar respuesta primera pregunta gastronomia
 function validar_respuesta_2() {
-
   clearInterval(identificadorTemporizador);
   temporizador = 61;
-
-  let respuesta_seleccionada2 = document.querySelector("input[name=guerra]:checked").value;
 
   let guardando = document.querySelector("#ver2");
 
-  if (respuesta_seleccionada2 === respuesta_correcta_2) {
-    guardando.innerHTML = ("Correcto!!!, la respuesta es: " + respuesta_seleccionada2);
-    document.getElementById("ver2").style.color = "green";
-
-    puntaje += 100;
-    document.getElementById("puntos2").innerHTML = puntaje + " " + "puntos";
-
-    acierto += 1;
-    document.getElementById("acierto-2").innerHTML = acierto + "/3";
-
-  } else {
-
-    guardando.innerHTML = ("Incorrecto, la respuesta correcta es :" + respuesta_correcta_2);
-    document.getElementById("ver2").style.color = "red";
-
-
-
+  // Si no se ha seleccionado ninguna respuesta, mostrar alerta
+  if (!respuesta_seleccionada) {
+    guardando.innerHTML = "⚠️ Por favor, selecciona una respuesta antes de validar.";
+    document.getElementById("ver2").style.color = "orange";
+    return;
   }
 
+  if (respuesta_seleccionada === respuesta_correcta_2) {
+    guardando.innerHTML = `✅ Correcto!!!, la respuesta es: ${respuesta_seleccionada}`;
+    document.getElementById("ver1").style.color = "green";
+    puntaje += 100;
+    document.getElementById("puntos-2").innerHTML = puntaje + " puntos";
+    acierto += 1;
+    document.getElementById("acierto-2").innerHTML = acierto + "/3";
+  } else {
+    guardando.innerHTML = `❌ Incorrecto, la respuesta correcta es: ${respuesta_correcta_2}`;
+    document.getElementById("ver2").style.color = "red";
+  }
 }
 
-function siguiente_2() {
-
-  document.getElementById("cultura_general_3").style.display = "block";
-  document.getElementById("cultura_general_2").style.display = "none";
-
-  identificadorTemporizador = setInterval(function () {
-    --temporizador;
-    document.getElementById("tiempo3").innerHTML = temporizador;
-    if (temporizador === 0) {
-      clearInterval(identificadorTemporizador);
-    }
+// boton ir a la siguiente pregunta
+document.getElementById('siguiente_2').onclick = function() {  
+  document.getElementById("two").style.display = "none";
+  document.getElementById('three').style.display = "block";
+};
 
 
-  }, 1000);
 
 
-}
 
+// la tercera pregunta de cultura
 function validar_respuesta_3() {
-
   clearInterval(identificadorTemporizador);
-
-  let respuesta_seleccionada3 = document.querySelector("input[name=invento]:checked").value;
+  temporizador = 61;
 
   let guardando = document.querySelector("#ver3");
 
-  if (respuesta_seleccionada3 === respuesta_correcta_3) {
-    guardando.innerHTML = ("Correcto!!!, la respuesta es: Thomas Edison");
+  // Si no se ha seleccionado ninguna respuesta, mostrar alerta
+  if (!respuesta_seleccionada) {
+    guardando.innerHTML = "⚠️ Por favor, selecciona una respuesta antes de validar.";
+    document.getElementById("ver3").style.color = "orange";
+    return;
+  }
+
+  if (respuesta_seleccionada === respuesta_correcta_3) {
+    guardando.innerHTML = `✅ Correcto!!!, la respuesta es: ${respuesta_seleccionada}`;
     document.getElementById("ver3").style.color = "green";
-
     puntaje += 100;
-    document.getElementById("puntos3").innerHTML = puntaje + " " + "puntos";
-
+    document.getElementById("puntos-3").innerHTML = puntaje + " puntos";
     acierto += 1;
     document.getElementById("acierto-3").innerHTML = acierto + "/3";
-
-
   } else {
-    guardando.innerHTML = ("Incorrecto, la respuesta correcta es  : Thomas Edison");
+    guardando.innerHTML = `❌ Incorrecto, la respuesta correcta es: ${respuesta_correcta_3}`;
     document.getElementById("ver3").style.color = "red";
-
-
-
   }
-
 }
 
+// boton ir a la siguiente pregunta
+document.getElementById('siguiente_3').onclick = function() {
+  document.getElementById("three").style.display = "none";
+  ocument.getElementById("culturaGeneral").style.display = "none"; // revisar
+  document.getElementById("contenedor-2").style.display = "block"; // mostrar otra vez la lista de opciones de juego
+  
 
-function siguiente_3() {
-
-  document.getElementById("contenedor-1").style.display = "block";
-  document.getElementById("cultura_general_3").style.display = "none";
   document.getElementById("ver-nombre-usuario").style.display = "none";
   document.getElementById("nombreUsuario").value = "";
-
-}
-
-function validar_respuesta_1_2() {
+};
 
 
-  clearInterval(identificadorTemporizador);
-  temporizador = 61;
-
-  let respuesta_seleccionada = document.querySelector("input[name=hojas]:checked").value;
-
-  let guardando = document.querySelector("#ver-1-1");
-
-  if (respuesta_seleccionada === respuesta_correcta_1_2) {
-    guardando.innerHTML = ("Correcto!!!, la respuesta es: " + respuesta_seleccionada);
-    document.getElementById("ver-1-1").style.color = "green";
-
-    puntaje += 100;
-    document.getElementById("puntosgas").innerHTML = puntaje + " " + "puntos";
-
-    acierto += 1;
-    document.getElementById("acierto-gastro1").innerHTML = acierto + "/3";
-
-  } else {
-    guardando.innerHTML = ("Incorrecto, la respuesta correcta es:" + respuesta_correcta_1_2);
-    document.getElementById("ver-1-1").style.color = "red";
-
-
-
-  }
-
-}
-
-function siguiente_2_1() {
-
-  document.getElementById("gastronomia-2").style.display = "block";
-  document.getElementById("gastronomia-1").style.display = "none";
-
-  identificadorTemporizador = setInterval(function () {
-    --temporizador;
-    document.getElementById("tiempo2_2").innerHTML = temporizador;
-
-    if (temporizador === 0) {
-      clearInterval(identificadorTemporizador);
-    }
-
-
-  }, 1000);
-
-}
-
-function validar_respuesta_2_2() {
-
-
-  clearInterval(identificadorTemporizador);
-  temporizador = 61;
-
-  let respuesta_seleccionada = document.querySelector("input[name=ceviche]:checked").value;
-
-  let guardando = document.querySelector("#ver-1-2");
-
-  if (respuesta_seleccionada === respuesta_correcta_2_2) {
-    guardando.innerHTML = ("Correcto!!!, la respuesta es: " + respuesta_seleccionada);
-    document.getElementById("ver-1-2").style.color = "green";
-
-    puntaje += 100;
-    document.getElementById("puntosgas2").innerHTML = puntaje + " " + "puntos";
-
-    acierto += 1;
-    document.getElementById("acierto-gastro2").innerHTML = acierto + "/3";
-
-  } else {
-    guardando.innerHTML = ("Incorrecto, la respuesta correcta es: " + respuesta_correcta_2_2);
-    document.getElementById("ver-1-2").style.color = "red";
-
-
-
-  }
-
-}
-
-function siguiente_2_2() {
-
-  document.getElementById("gastronomia-3").style.display = "block";
-  document.getElementById("gastronomia-2").style.display = "none";
-
-  identificadorTemporizador = setInterval(function () {
-    --temporizador;
-    document.getElementById("tiempo3_2").innerHTML = temporizador;
-    if (temporizador === 0) {
-      clearInterval(identificadorTemporizador);
-    }
-
-
-  }, 1000);
-}
-
-function validar_respuesta_3_2() {
-
-  clearInterval(identificadorTemporizador);
-  temporizador = 61;
-
-  let respuesta_seleccionada = document.querySelector("input[name=guiso]:checked").value;
-
-  let guardando = document.querySelector("#ver-1-3");
-
-  if (respuesta_seleccionada === respuesta_correcta_3_2_2) {
-    guardando.innerHTML = ("Correcto!!!, la respuesta  es: " + respuesta_seleccionada);
-    document.getElementById("ver-1-3").style.color = "green";
-
-    puntaje += 100;
-    document.getElementById("puntosgas3").innerHTML = puntaje + " " + "puntos";
-
-    acierto += 1;
-    document.getElementById("acierto-gastro3").innerHTML = acierto + "/3";
-
-  } else {
-    guardando.innerHTML = ("Incorrecto, la respuesta correcta es: " + respuesta_correcta_3_2_2);
-    document.getElementById("ver-1-3").style.color = "red";
-
-
-
-  }
-
-}
-
-function siguiente_2_3() {
-
-  document.getElementById("contenedor-1").style.display = "block";
-  document.getElementById("gastronomia-3").style.display = "none";
-  document.getElementById("ver-nombre-usuario").style.display = "none";
-
-  document.getElementById("nombreUsuario").value = "";
-}
